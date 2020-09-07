e44ee70facfb6f29ce6ff6823a112edeab389a9c
David Chou <mrycchou@gmail.com>
2020-09-07 15:13:23 -0700

    Initialize default state in GomokuState

--- src/main/games/gomoku/GomokuState.java
+++ src/main/games/gomoku/GomokuState.java
@@ -4,6 +4,26 @@ import main.games.Player;
 import main.games.State;
 
 public class GomokuState implements State {
+    private static int boardSize = 15 * 15;
+    private static Player[] defaultState;
+    private final Player[] state;
+    
+    static {
+        Player[] state = new Player[boardSize];
+        for (int i = 0; i < state.length; i++) {
+            state[i] = Player.EMPTY;
+        }
+        defaultState = state;
+    }
+
+    public GomokuState() {
+        this(defaultState);
+    }
+
+    public GomokuState(Player[] state) {
+        this.state = state;
+    }
+
     @Override
     public Player getPositionAt(int index) {
         return null;
